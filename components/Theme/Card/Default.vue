f100144f1824ed66f6169b1ee6a06527e88298ca
harian12 <hariansyah434@gmail.com>
2024-07-10 11:12:11 +0700

    sidebar admin dash

---
+++ components/Theme/Card/Default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+  >
+    <h5
+      class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+    >
+      <slot name="header"></slot>
+    </h5>
+    <div class="font-normal text-gray-700 dark:text-gray-400">
+      <slot name="body"></slot>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style></style>
---
+++ layouts/admin.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="h-auto w-auto">
+    <Sidebar :menu="menu" />
+    <div class="sm:ml-64">
+      <div class="w-full h-14"></div>
+      <div class="px-4 pb-4">
+        <NuxtPage />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import type { typeMenu } from "@/utils/utilTypes";
+
+onMounted(() => {
+  useFlowbite(() => {
+    // @ts-ignore
+    initFlowbite();
+  });
+
+});
+
+const menu: Array<typeMenu> = [
+  {
+    name: "dashboard",
+    to: "/admin/dashboard",
+    dropdown: false,
+    icon: "mingcute:dashboard-fill",
+    subMenu: [],
+  },
+  {
+    name: "pages",
+    to: "/pages",
+    dropdown: true,
+    icon: "ri:pages-fill",
+    subMenu: [
+      {
+        name: "setting",
+        to: "/admin/pages/setting",
+        icon: "ant-design:setting-filled",
+      },
+      { name: "kanban", to: "/admin/pages/kanban", icon: "bi:kanban-fill" },
+      { name: "calender", to: "/admin/pages/calender", icon: "uis:calender" },
+    ],
+  },
+  {
+    name: "cards",
+    to: "/admin/cards",
+    dropdown: false,
+    icon: "solar:card-bold",
+    subMenu: [],
+  },
+];
+</script>
+<style lang=""></style>
